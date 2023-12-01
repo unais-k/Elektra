b59c44f928b0d4d99a04ed9e8aa88bb380a7fff6
Unais Kattungal <105600128+unais-k@users.noreply.github.com>
2023-12-01 11:43:27 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ mongoose
     .connect(process.env.DatabaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => {
         // If connection is successful, start the server.
-        console.log("Port connected & Server at 3000");
+        console.log("mongoDB connected");
         // app.listen(3000, () => {
         // });
     })
